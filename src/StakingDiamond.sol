149ef077dbac1325b81305f5b086cf27a4f011b0
mektigboy <mektigboy@protonmail.com>
2022-11-28 15:39:00 +0800

    feat: update staking diamond contract

---
+++ src/StakingDiamond.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "clouds/src/interfaces/IDiamondCut.sol";
+import "clouds/src/diamond/LDiamond.sol";
+import "./libraries/AppStorage.sol";
+
+error StakingDiamond__InvalidFunction();
+
+/// @title StakingDiamond
+/// @author mektigboy
+/// @author Modified from Nick Mudge: https://github.com/mudgen/diamond-3-hardhat
+/// @notice Main contract of the diamond
+/// @dev Utilizes 'IDiamondCut', 'LDiamond' and 'AppStorage'
+contract StakingDiamond {
+    /////////////
+    /// LOGIC ///
+    /////////////
+
+    constructor(address _owner, address _diamondCutFacet) payable {
+        LDiamond.updateContractOwner(_owner);
+
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+
+        bytes4[] memory functionSelectors = new bytes4[](1);
+
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: _diamondCutFacet,
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: functionSelectors
+        });
+
+        LDiamond.diamondCut(cut, address(0), "");
+    }
+
+    fallback() external payable {
+        LDiamond.DiamondStorage storage ds;
+
+        bytes32 position = LDiamond.DIAMOND_STORAGE_POSITION;
+
+        assembly {
+            ds.slot := position
+        }
+
+        address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
+
+        if (facet == address(0)) revert StakingDiamond__InvalidFunction();
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(gas(), facet, 0, calldatasize(), 0, 0)
+
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
+    }
+}
