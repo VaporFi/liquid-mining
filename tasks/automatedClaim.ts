108f6890997e73bffb8d339d4acf63c65c85b732
MejiasDev <josemejiasji@gmail.com>
2023-06-21 00:31:03 +0200

    feat: add github actions for automatedClaim task

--- tasks/automatedClaim.ts
+++ tasks/automatedClaim.ts
@@ -1,10 +1,23 @@
 import { task } from 'hardhat/config'
 import LiquidMiningDiamond from '../deployments/LiquidMiningDiamond.json'
 import chunk from '../utils/chunk'
+import { DepositEvent } from '../typechain-types/src/facets/DepositFacet'
+import Bottleneck from 'bottleneck'
+
+const FROM_BLOCK: { [key: string]: number } = {
+  fuji: 23009545,
+  avalanche: 0, // TODO: Update this once we have a mainnet deployment
+}
+
+const limiter = new Bottleneck({
+  minTime: 33, // ~30 requests per second
+  maxConcurrent: 1,
+})
 
 task('automatedClaim', 'Claim all rewards for a season')
   .addParam('seasonId', 'The season ID')
-  .setAction(async ({ seasonId }, { ethers, network }) => {
+  .addOptionalParam('dryRun', 'Whether to run the task without claiming')
+  .setAction(async ({ seasonId, dryRun }, { ethers, network }) => {
     // Load Diamond and ClaimFacet
     const diamondAddress =
       LiquidMiningDiamond[network.name as keyof typeof LiquidMiningDiamond]
@@ -16,9 +29,28 @@ task('automatedClaim', 'Claim all rewards for a season')
     const ClaimFacet = await ethers.getContractAt('ClaimFacet', diamondAddress)
     // Load all Deposit events
     const filter = DepositFacet.filters.Deposit(seasonId, null, null)
-    const depositEvents = await DepositFacet.queryFilter(filter)
+    /*
+     * Query events for evey 10,000 blocks since FROM_BLOCK
+     * using a limiter to avoid rate limiting from RPC provider
+     * @dev Ankr has a rate limit of 30 requests per second
+     */
+    const lastBlock = await ethers.provider.getBlockNumber()
+    const result = await limiter.schedule(async () => {
+      let depositEvents: DepositEvent[] = []
+      for (let i = FROM_BLOCK[network.name]; i < lastBlock; i += 10_000) {
+        const toBlock = i + 10_000 > lastBlock ? 'latest' : i + 10_000
+        console.log(`Querying events from block ${i} to ${toBlock}`)
+        const events = await DepositFacet.queryFilter(filter, i, toBlock)
+        console.log(`Found ${events.length} events`)
+        depositEvents = depositEvents.concat(events)
+      }
+      console.log(`Found ${depositEvents.length} total events`)
+      return depositEvents
+    })
+
     // Get all depositors
-    const depositors = depositEvents.map((event) => event.args?.user)
+    if (dryRun === 'true') return // If dry run, return early
+    const depositors = result.map((event) => event.args?.user)
     const uniqueDepositors = [...new Set(depositors)]
     // Slice the array into chunks of 100
     const chunkedDepositors = chunk(uniqueDepositors, 100)
