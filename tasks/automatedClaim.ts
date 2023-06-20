72ec67f9fa4393405ab3ecc73953d454f3fca6a9
MejiasDev <josemejiasji@gmail.com>
2023-06-21 00:36:09 +0200

    chore: cleanup

--- tasks/automatedClaim.ts
+++ tasks/automatedClaim.ts
@@ -52,10 +52,26 @@ task('automatedClaim', 'Claim all rewards for a season')
     if (dryRun === 'true') return // If dry run, return early
     const depositors = result.map((event) => event.args?.user)
     const uniqueDepositors = [...new Set(depositors)]
-    // Slice the array into chunks of 100
-    const chunkedDepositors = chunk(uniqueDepositors, 100)
-    // Claim for each chunk
-    for (const chunk of chunkedDepositors) {
-      await (await ClaimFacet.automatedClaim(seasonId, chunk)).wait(3)
+
+    try {
+      // Slice the array into chunks of 100
+      const chunkedDepositors = chunk(uniqueDepositors, 100)
+      // Claim for each chunk
+      for (const chunk of chunkedDepositors) {
+        await (await ClaimFacet.automatedClaim(seasonId, chunk)).wait(3)
+      }
+    } catch (e) {
+      console.error(e)
+      throw e
     }
+
+    /*
+     * TODO: implement starting a new season right after claiming
+     * 1. Calculate new season rewards amount (VAPE)
+     * 2. Calculate new season start and end timestamps
+     * 3. Call EmmissionsManager to mint VAPE
+     * 4. Call Diamond to start new season
+     */
+
+    console.log('✅ Done')
   })
