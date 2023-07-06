89f11b841d758ba60ef94a62366fc68081267aea
MejiasDev <josemejiasji@gmail.com>
2023-07-07 01:30:29 +0200

    feat: deploy to avalanche

--- tasks/verify.ts
+++ tasks/verify.ts
@@ -1,5 +1,6 @@
 import { task } from 'hardhat/config'
 import getContractDeployed from '../utils/getContractDeployment'
+import getFacets from '../utils/getFacets'
 
 task('verify:contract', 'Verifies a contract on etherscan')
   .addParam('contract', 'The contract name')
@@ -24,21 +25,7 @@ task('verify:facets', 'Verifies all facets on etherscan').setAction(
   async (taskArgs, hre) => {
     const { network } = hre
 
-    const facets = [
-      'AuthorizationFacet',
-      'BoostFacet',
-      'ClaimFacet',
-      'DepositFacet',
-      'DiamondCutFacet',
-      'DiamondLoupeFacet',
-      'DiamondManagerFacet',
-      'FeeCollectorFacet',
-      'OwnershipFacet',
-      'PausationFacet',
-      'RestakeFacet',
-      'UnlockFacet',
-      'WithdrawFacet',
-    ]
+    const facets = getFacets([]) // empty array to omit no facets
 
     for (const facet of facets) {
       const { address, args } = await getContractDeployed(facet, network.name)
