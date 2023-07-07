1b8f8e29abced119ac1e62365eb81f6bb6926649
Hitesh Agrawal <hitesh@vaporfi.io>
2023-07-07 13:45:13 +0530

    fix: incorrect boost fee config

--- src/upgradeInitializers/DiamondInit.sol
+++ src/upgradeInitializers/DiamondInit.sol
@@ -113,8 +113,8 @@ contract DiamondInit {
         s.boostFeeReceivers.push(_args.labsMultisig);
         s.boostFeeReceivers.push(_args.xVAPE);
         s.boostFeeReceivers.push(_args.passport);
-        s.boostFeeReceiversShares.push(5000); // 50%
-        s.boostFeeReceiversShares.push(4000); // 40%
+        s.boostFeeReceiversShares.push(6000); // 50%
+        s.boostFeeReceiversShares.push(3000); // 40%
         s.boostFeeReceiversShares.push(1000); // 10%
 
         // MiningPassFacet
